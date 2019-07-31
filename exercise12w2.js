39d2d81680ab655ca02ddbd579aaf51bc62caf7f
hoandreas <andreas_ho@hotmail.com>
2019-08-01 01:24:06 +0700

    edit exercise12w2.js

--- exercise12w2.js
+++ exercise12w2.js
@@ -1,12 +1,12 @@
 function konversiMenit(menit) {
     var hour = Math.floor(menit/60);
-    var minutes = menit % 60;
+    var minutes = menit-(hour*60);
     if(minutes < 10) {
         minutes = '0' + minutes;
     }
     return hour + ":" + minutes;
   }
-  
+
   // TEST CASES
   console.log(konversiMenit(63)); // 1:03
   console.log(konversiMenit(124)); // 2:04
