a7108d9b141683aeb22d9c422348fec0ce40edb3
hoandreas <andreas_ho@hotmail.com>
2019-08-03 13:45:54 +0700

    improved exercise9w2.js

--- exercise9w2.js
+++ exercise9w2.js
@@ -17,7 +17,7 @@ var hasilPerkalian = calculateMultiply(num1,num2);
 console.log(hasilPerkalian);
 
 // TUGAS 3
-function processSentence() {
+function processSentence(name, age, address, hobby) {
     return "Nama saya " + name + " umur saya " + age + " tahun," + " alamat saya di " + address + "," + " dan saya punya hobby yaitu " + hobby + '!';
 }
 var name = "Agus";
@@ -25,5 +25,5 @@ var age = 30;
 var address = "Jln. Malioboro, Yogjakarta";
 var hobby = "gaming";
 
-var fullSentence = processSentence(name,age,address,hobby);
+var fullSentence = processSentence(name, age, address, hobby);
 console.log(fullSentence);
